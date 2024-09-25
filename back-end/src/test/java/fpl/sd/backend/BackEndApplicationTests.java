21db4e01d96ad7176352ff045bd4c64cfa6460f4
FixLuck <caube.vodoi5@gmail.com>
2024-09-25 16:03:34 +0700

    first commit

---
+++ back-end/src/test/java/fpl/sd/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package fpl.sd.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ front-end/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <div className="mt-16 px-20">
+        <Button>Click me</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;

555517e4b2ab6ba8596767f048ee67a4809da618
anhkhoicao <fuggboi025@gmail.com>
2024-10-18 00:00:13 +0700

    modify WelcomeAdmin

---
+++ front-end/src/pages/WelcomeAdmin.jsx
@@ -0,0 +1,7 @@
+export function WelcomeAdmin() {
+    return (
+        <div>
+            <h1>Welcome Admin</h1>
+        </div>
+    );
+}
---
+++ front-end/src/pages/WelcomeManager.jsx
@@ -0,0 +1,7 @@
+export function WelcomeManager() {
+    return (
+        <div>
+            <h1>Welcome Manager</h1>
+        </div>
+    );
+}

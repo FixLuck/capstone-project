d4676adbd075d97dd574cec606ba41129f123f66
FixLuck <caube.vodoi5@gmail.com>
2024-10-21 17:31:33 +0700

    add fe and be

---
+++ front-end/src/components/layout/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import UserDropDown from "../shop/UserDropDown";
+import ShoppingBag from "../shop/ShoppingBag";
+
+export default function Header() {
+  return (
+    <header className="fixed top-0 w-full z-50 bg-white shadow">
+      <div className="container flex items-center justify-between mx-auto gap-5 h-24">
+        <Link to={"/"}>
+          <h1 className="text-red-800 text-4xl italic font-bold">SuperTeam</h1>
+        </Link>
+        <div className="hidden lg:flex gap-12 justify-between">
+          <Link to={"/"}>Home</Link>
+          <Link to={"/shoes"}>Products</Link>
+          <Link to={"/add-shoe"}>Add Shoe</Link>
+          <Link to={"/users"}>Users</Link>
+          <Link to={"/posts"}>Posts</Link>
+        </div>
+        <div className="flex gap-5">
+          <UserDropDown/>
+          <ShoppingBag/>
+        </div>
+      </div>
+    </header>
+  );
+}

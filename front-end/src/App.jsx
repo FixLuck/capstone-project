cf114956f2ee9cffa6a725752f86c1dbcce49136
FixLuck <caube.vodoi5@gmail.com>
2024-11-12 20:44:52 +0700

    fix conflict

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -22,74 +22,9 @@ import MemberOrderHistory from "./pages/admin-pages/MemberOrderHistory";
 import WelcomeManager from "./pages/manager-pages/WelcomeManager";
 import DetailShoePage from "./pages/shop-pages/DetailShoePage";
 import ListManageShoePage from "./pages/admin-pages/ListManageShoePage";
-
-import CheckOut from "./pages/shop-pages/CheckOut";
-
-
-    const router = createBrowserRouter([
-      {
-        path: "/",
-        element: <RootLayout/>,
-        errorElement: <ErrorPage/>,
-        children: [
-          
-        {path: 'shoes', element: <ListShoePage/>},
-        {path: 'shoes/:id', element: <DetailShoePage/>},
-        {path: 'add-shoe', element: <AddShoePage/>},
-        {path: 'cart', element: <Cart/>},
-        {path: 'order-history', element: <OrderHistory/>},
-        {path: 'login', element: <UserLogin/>},
-        {path: 'register', element: <SignUp/>},
-        {path: 'users', element: <Profile/>},
-        {path: 'orders', element: <OrderHistory/>},
-        {path: 'checkout', element: <CheckOut/>},
-        {path: 'logout', element: <UserLogin/>}
-        ]
-      },
-      {
-        path: "/admin",
-        element: <RootLayoutAdmin/>,
-        errorElement: <ErrorPage/>,
-        children: [
-        {index: true, element: <WelcomeAdmin/>},
-        {path: 'discount-management', element: <DiscountManagement/>},
-        {path: 'member-order-history', element: <MemberOrderHistory/>},
-        {path: 'account-management', element: <MemberManagemant/>},
-        {path: 'revenue-stats', element: <RevenueStatistics/>},
-
-        {path: 'manage-shoes', element: <ListManageShoePage/>},
-        {path: 'manage-shoes/new', element: <AddShoePage/>},
-
-        {path: 'profile', element: <Profile/>},
-          
-
 import AddDiscountPage from "./pages/admin-pages/AddDiscountPage";
-
-
-
 import CheckOut from "./pages/shop-pages/CheckOut";
 
-
-        ]
-      },
-    ]);
-
-
-function App() {
-
-  return (
-    
-
-    <RouterProvider router={router}>
-
-    </RouterProvider>
-
-
-  )
-
-
-
-  
 
 const router = createBrowserRouter([
   {

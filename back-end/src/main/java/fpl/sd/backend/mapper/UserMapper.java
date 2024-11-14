3f07091d5c0a629a90d2c07a6fa71d8b7732b43e
FixLuck <caube.vodoi5@gmail.com>
2024-11-14 15:48:11 +0700

    fix bug be

--- back-end/src/main/java/fpl/sd/backend/mapper/UserMapper.java
+++ back-end/src/main/java/fpl/sd/backend/mapper/UserMapper.java
@@ -1,13 +1,19 @@
 package fpl.sd.backend.mapper;
 import fpl.sd.backend.dto.request.UserCreateRequest;
+import fpl.sd.backend.dto.request.UserUpdateRequest;
 import fpl.sd.backend.dto.response.UserResponse;
 import fpl.sd.backend.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+
     User toUser(UserCreateRequest request);
 
     UserResponse toUserResponse(User user);
+
+    @Mapping(source = "active", target = "isActive")
+    User toUser(UserUpdateRequest request);
 }

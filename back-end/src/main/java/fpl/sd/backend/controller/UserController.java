4444f19e30e54cf73a24ab24ba357220ba0377b0
anhkhoicao <fuggboi025@gmail.com>
2024-11-14 16:50:00 +0700

    commit

--- back-end/src/main/java/fpl/sd/backend/controller/UserController.java
+++ back-end/src/main/java/fpl/sd/backend/controller/UserController.java
@@ -64,10 +64,5 @@ public class UserController {
                 .result(updateUser)
                 .build();
     }
-<<<<<<< HEAD
 
-
-
-=======
->>>>>>> f37317a75e18a5c47fbf9e6e0725dba6e912b896
 }
